4ec44a146a6b7785a7fa602fc9721e99c11e912e
Alexander McComb <oero.mccomb@gmail.com>
2021-10-01 10:31:33 -0500

    implemented first functions to calculate points

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,20 @@
 const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.calculatePoints = functions.firestore
+    .document('users/{docId}/questionsAnswered')
+    .onWrite((change, context) => {
+        console.log(context);
+        let beforePoints = change.before.data().achievementPoints;
+        let answered = change.after.data().questionsAnswered;
+        
+        answered.forEach(question => {
+            admin.firestore.CollectionReference('theOfficeTriviaQuestions').document(question)
+                .get().then((doc) => {
+                    beforePoints = beforePoints + doc.data().difficulty;
+                })
+        })
+
+        admin.firestore.CollectionReference('users').document(change.id).update({achievementPoints: beforePoints});
+    })
