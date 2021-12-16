e1657680cb2de1b876fc21405bd72da9d88938fd
Alexander McComb <oero.mccomb@gmail.com>
2021-12-15 21:45:45 -0600

    updated cloud functions

--- functions/index.js
+++ functions/index.js
@@ -4,13 +4,14 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
-exports.calculatePoints = functions.firestore.document('/users/{docId}/questions-answered/{docId}')
+exports.addNewPoints = functions.firestore.document('/users/{docId}/questions-answered/{docIdTwo}')
     .onCreate((snap, context) => {
         var userRef = db.collection('users').doc(context.params.docId);
         var newPoints;
         userRef.get().then(doc => {
             newPoints = doc.data().achievementPoints + (snap.data().difficulty * 10);
-            functions.logger.log(newPoints);
+            
             return userRef.update({achievementPoints: newPoints});
         })
+        return "adding new points";
     })
